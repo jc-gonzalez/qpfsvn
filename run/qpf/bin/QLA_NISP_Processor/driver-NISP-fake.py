044b9966484cd3c47233a7ce374715004d95d5bf
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2018-03-04 20:44:49 +0000

    Corrected QLA_VIS & QLA_NISP Processor fake drivers
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/branches/V2.0@19620 463500fe-d6be-4184-9b20-47717e75ecac

--- run/qpf/bin/QLA_NISP_Processor/driver-NISP-fake.py
+++ run/qpf/bin/QLA_NISP_Processor/driver-NISP-fake.py
@@ -14,6 +14,7 @@ import argparse
 import json
 import logging
 import glob
+import os
 
 
 __author__ = 'jcgonzalez' # QDT fake
@@ -63,14 +64,30 @@ def main():
     input_files = glob.glob(args.input_dir + "/*.fits")
     logger.debug("Input files: %s", ''.join(input_files))
 
+    for fits_file in input_files:
+        hdulist = fits.open(fits_file)
+        hnfo = hdulist.info(output=False)
+        hnfo.insert(0,('----','-------','-------','------',
+                       '-------------','-------'))
+        hnfo.insert(0,('No.','Name','Type','Cards','Dimensions','Format'))
+        for t in hnfo:
+            tt = map(str, t)
+            logger.info('{0:4s} {1:10s} {2:16s} {3:8s} {4:16s} {5:8s}'.format(*tt))
+
+        hdu_no = 0
+        for hdu in hdulist:
+            logger.info("HDU # {0}:".format(hdu_no))
+            for hkey, hval in hdu.header.items():
+                logger.info("{0:10s} : {1}".format(hkey, hval))
+            hdu_no = hdu_no + 1
+        hdulist.close()
+
     numSteps  = 40
     moreSteps = 10
 
     progress      = 0.0
     endProgress   = 100.0
     deltaProgress = endProgress / numSteps
-
-    fres.write("Starting processing . . .\n")
 
     logger.info("Starting processing . . .")
 
@@ -92,25 +109,303 @@ def main():
             numSteps += moreSteps
             deltaProgress = (endProgress - progress) / (numSteps - step)
 
-    for fits_file in input_files:
-        hdulist = fits.open(fits_file)
-        hdulist.info(output=fres)
-        hdu_no = 0
-        for hdu in hdulist:
-            fres.write("HDU # {}:\n".format(hdu_no))
-            for hkey, hval in hdu.header.items():
-                fres.write("{:10s} : {}\n".format(hkey, hval))
-            hdu_no = hdu_no + 1
-        hdulist.close()
+    resFile = os.path.basename(args.result_file)
+    report = {
+        resFile: {
+            "CCD_1-6": {
+                "Q-1": {
+                    "diagnostics": {
+                        "Electronic_Offset": {
+                            "name": "Electronic_Offset",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 200.02508798391153
+                            }
+                        },
+                        "Overflow_Pixels": {
+                            "name": "Overflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Readout_Noise": {
+                            "name": "Readout_Noise",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 1.4694961041027172
+                            }
+                        },
+                        "Saturated_pixels": {
+                            "name": "Saturated_pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Saturation_Level": {
+                            "name": "Saturation_Level",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "map_ADU": [
+                                    [ 1297.0, 464.0, 202.0, 160536.59375, 173045.09375 ],
+                                    [ 1296.0, 464.5, 61.0, 159436.09375, 172468.5 ],
+                                    [ 1298.0, 464.5, 7.0, 172322.796875, 172759.90625 ],
+                                    [ 1605.0, 1661.5, 35.0, 156940.59375, 171814.40625 ],
+                                    [ 1606.0, 1661.5, 7.0, 171898.09375, 172970.703125 ]
+                                ]
+                            }
+                        },
+                        "Statistics": {
+                            "name": "Statistics",
+                            "result": {
+                                "messages": [ "Std.Dev. threshold (150) exceeded" ],
+                                "outcome": "Warning"
+                            },
+                            "values": {
+                                "average": -2332.303377663727,
+                                "median": -2418.0,
+                                "std": 1641.5975057454245
+                            }
+                        },
+                        "Underflow_Pixels": {
+                            "name": "Underflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        }
+                    },
+                    "processing": {
+                        "BIAS_COR": {
+                            "value": 1000.0
+                        },
+                        "GAIN_COR": {
+                            "value": 3.1
+                        }
+                    }
+                },
+                "Q-2": {
+                    "diagnostics": {
+                        "Electronic_Offset": {
+                            "name": "Electronic_Offset",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 200.01892911010557
+                            }
+                        },
+                        "Overflow_Pixels": {
+                            "name": "Overflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Readout_Noise": {
+                            "name": "Readout_Noise",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 1.478162114445415
+                            }
+                        },
+                        "Saturated_pixels": {
+                            "name": "Saturated_pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Saturation_Level": {
+                            "name": "Saturation_Level",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "map_ADU": [
+                                    [ 1148.0, 27.0, 8.0, 161461.953125, 165933.703125 ]
+                                ]
+                            }
+                        },
+                        "Statistics": {
+                            "name": "Statistics",
+                            "result": {
+                                "messages": [ "Std.Dev. threshold (150) exceeded" ],
+                                "outcome": "Warning"
+                            },
+                            "values": {
+                                "average": -2347.976286844222,
+                                "median": -2418.0,
+                                "std": 792.1386082283798
+                            }
+                        },
+                        "Underflow_Pixels": {
+                            "name": "Underflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        }
+                    },
+                    "processing": {
+                        "BIAS_COR": {
+                            "value": 1000.0
+                        },
+                        "GAIN_COR": {
+                            "value": 3.1
+                        }
+                    }
+                },
+                "Q-3": {
+                    "diagnostics": {
+                        "Electronic_Offset": {
+                            "name": "Electronic_Offset",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 200.02735042735043
+                            }
+                        },
+                        "Overflow_Pixels": {
+                            "name": "Overflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Readout_Noise": {
+                            "name": "Readout_Noise",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 1.480563748079826
+                            }
+                        },
+                        "Saturated_pixels": {
+                            "name": "Saturated_pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Saturation_Level": {
+                            "name": "Saturation_Level",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "map_ADU": [
+                                    [ 262.0, 1177.5, 59.0, 169914.09375, 173082.296875 ],
+                                    [ 261.0, 1177.0, 6.0, 170227.203125, 170689.09375 ]
+                                ]
+                            }
+                        },
+                        "Statistics": {
+                            "name": "Statistics",
+                            "result": {
+                                "messages": [ "Std.Dev. threshold (150) exceeded" ],
+                                "outcome": "Warning"
+                            },
+                            "values": {
+                                "average": -2337.071306827887,
+                                "median": -2418.0,
+                                "std": 1113.4418961469928
+                            }
+                        },
+                        "Underflow_Pixels": {
+                            "name": "Underflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        }
+                    },
+                    "processing": {
+                        "BIAS_COR": {
+                            "value": 1000.0
+                        },
+                        "GAIN_COR": {
+                            "value": 3.1
+                        }
+                    }
+                },
+                "Q-4": {
+                    "diagnostics": {
+                        "Electronic_Offset": {
+                            "name": "Electronic_Offset",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 200.03288084464555
+                            }
+                        },
+                        "Overflow_Pixels": {
+                            "name": "Overflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Readout_Noise": {
+                            "name": "Readout_Noise",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "value": 1.4760630140943263
+                            }
+                        },
+                        "Saturated_pixels": {
+                            "name": "Saturated_pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        },
+                        "Saturation_Level": {
+                            "name": "Saturation_Level",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "map_ADU": [
+                                    [ 603.0, 97.0, 14.0, 166784.65625, 173321.0 ],
+                                    [ 1551.0, 464.0, 54.0, 157662.90625, 171767.90625 ],
+                                    [ 1552.0, 464.0, 8.0, 171560.203125, 173001.703125 ],
+                                    [ 1975.0, 2011.0, 6.0, 153977.0, 157563.703125 ]
+                                ]
+                            }
+                        },
+                        "Statistics": {
+                            "name": "Statistics",
+                            "result": {
+                                "messages": [ "Std.Dev. threshold (150) exceeded" ],
+                                "outcome": "Warning"
+                            },
+                            "values": {
+                                "average": -2344.572935556028,
+                                "median": -2418.0,
+                                "std": 1085.328809959921
+                            }
+                        },
+                        "Underflow_Pixels": {
+                            "name": "Underflow_Pixels",
+                            "result": { "outcome": "Nominal" },
+                            "values": {
+                                "number": 0
+                            }
+                        }
+                    },
+                    "processing": {
+                        "BIAS_COR": {
+                            "value": 1000.0
+                        },
+                        "GAIN_COR": {
+                            "value": 3.1
+                        }
+                    }
+                },
+                "diagnostics": {
+                }
+            }
+        }
+    }
 
-    fres.write("Processing finished.\n")
-    fres.close()
-
+    with open(args.result_file, 'w') as fj:
+        json.dump(report, fj)
+            
     elapsed_time = time() - start_time
 
     logger.info("Processing finished.")
     logger.info("Execution time: {0:.2f} s".format(elapsed_time))
-
 
 
 if __name__ == "__main__":

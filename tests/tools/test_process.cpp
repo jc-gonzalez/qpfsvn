3d46f7d840690a9d9b8c0119566cc2510f8deb7f
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2018-07-19 11:47:00 +0000

    Test sample3 split in 3a and 3b
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@19978 463500fe-d6be-4184-9b20-47717e75ecac

--- tests/tools/test_process.cpp
+++ tests/tools/test_process.cpp
@@ -10,7 +10,7 @@ namespace Testprocess {
 
 TEST_F(Testprocess, Test_sample1) {
 
-    procxx::process ping( "ping", "www.google.com", "-c", "2" );
+    procxx::process ping( "ping", "www.google.com", "-c", "3" );
     ping.exec();
 
     std::string line;
@@ -37,10 +37,10 @@ TEST_F(Testprocess, Test_sample1) {
 TEST_F(Testprocess, Test_sample2) {
 
     // construct a child process that runs `cat`
-    procxx::process cat{"cat"};
+    procxx::process cat("cat");
     
     // construct a child process that runs `wc -c`
-    procxx::process wc{"wc", "-c"};
+    procxx::process wc("wc", "-c");
     
     // set up the pipeline and execute the child processes
     (cat | wc).exec();
@@ -60,36 +60,37 @@ TEST_F(Testprocess, Test_sample2) {
     SUCCEED();
 }
 
-TEST_F(Testprocess, Test_sample3) {
+TEST_F(Testprocess, Test_sample3a) {
 
     // OPTION 1: same limits for all processes in the pipeline
-    {
-        procxx::process cat{"cat"};
-        procxx::process wc{"wc", "-c"};
-        
-        procxx::process::limits_t limits;
-        limits.cpu_time(3);         // 3 second execution time limit
-        limits.memory(1024*1024*1); // 1 MB memory usage limit
-        (cat | wc).limit(limits).exec();
-    }
+    procxx::process cat("cat");
+    procxx::process wc("wc", "-c");
     
+    procxx::process::limits_t limits;
+    limits.cpu_time(3);         // 3 second execution time limit
+    limits.memory(1024*1024*1); // 1 MB memory usage limit
+    (cat | wc).limit(limits).exec();
+    
+    SUCCEED();
+}
+
+TEST_F(Testprocess, Test_sample3b) {
+
     // OPTION 2: individual limits for each process
-    {
-        procxx::process cat{"cat"};
-        procxx::process wc{"wc", "-c"};
-        
-        procxx::process::limits_t limits_wc;
-        limits_wc.cpu_time(3);         // 3 second execution time limit
-        limits_wc.memory(1024*1024*1); // 1 MB memory usage limit
-        wc.limit(limits_wc);
-        
-        procxx::process::limits_t limits_cat;
-        limits_cat.cpu_time(1);  // 1 second execution time limit
-        limits_cat.memory(1024); // 1 KB memory usage limit
-        cat.limit(limits_cat);
-        
-        (cat | wc).exec();
-    }
+    procxx::process cat("cat");
+    procxx::process wc("wc", "-c");
+    
+    procxx::process::limits_t limits_wc;
+    limits_wc.cpu_time(3);         // 3 second execution time limit
+    limits_wc.memory(1024*1024*1); // 1 MB memory usage limit
+    wc.limit(limits_wc);
+    
+    procxx::process::limits_t limits_cat;
+    limits_cat.cpu_time(1);  // 1 second execution time limit
+    limits_cat.memory(1024); // 1 KB memory usage limit
+    cat.limit(limits_cat);
+    
+    (cat | wc).exec();
     
     SUCCEED();
 }
